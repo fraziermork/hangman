a771eb42e3e2153a3a56a83e57966ea1b22c3492
Frazier Mork <fraziermork@gmail.com>
2016-01-12 22:26:21 -0800

    completed basic functionality to add classes and to play again once you win or lose

--- hangman.js
+++ hangman.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-var wordList = ['guavas']; //all letters lowercase
+var wordList = ['guavas', 'pizza', 'awkward', 'akimbo', 'fuchsia', 'glyph', 'pyramid', 'nostril', 'mnemonic']; //all letters lowercase
 
 var letterList = [['a','b', 'c', 'd', 'e','f','g','h','i','j','k','l','m'],['n','o', 'p', 'q', 'r','s','t','u','v','w','x','y','z']];
 var guessedLetters=[];
@@ -11,22 +11,28 @@ var guess;
 var numWrongGuesses = 0;
 var maxGuesses = 10;
 var divcount = 0;
+var spriteIncrement = 315;
 
-//set up the keyboard
-for (i=0; i < letterList[1].length; i++ ){
-  $('#topRow').append('<div class="letterHolder clickable" id=' + letterList[0][i] + '><p class="letter">'+ letterList[0][i] +'</p></div>');
-  $('#botRow').append('<div class="letterHolder clickable" id=' + letterList[1][i] + '><p class="letter">'+ letterList[1][i] +'</p></div>');
-}
+
+
 
 function play() {
 		//clear garbage from previous runs
 		myReset();
+
+    //set up the keyboard
+    for (i=0; i < letterList[1].length; i++ ){
+      $('#topRow').append('<div class="letterHolder clickable" id=' + letterList[0][i] + '><p class="letter">'+ letterList[0][i] +'</p></div>');
+      $('#botRow').append('<div class="letterHolder clickable" id=' + letterList[1][i] + '><p class="letter">'+ letterList[1][i] +'</p></div>');
+    }
 
     //initialize word to use, separate letters into an array
 		myWord = wordList[Math.floor(Math.random()*wordList.length)];
 		for (i=0; i<myWord.length; i++){
 			myWordArray.push(myWord.slice(i,i+1));
 		};
+    console.log(myWordArray);
+
 		//make divs for letters to go in
 		for(var i = 0; i < myWordArray.length; i++){
 			var newClass = "letter" + myWordArray[i];
@@ -43,21 +49,26 @@ function play() {
       if (inLettersArrayFlag === false){
         lettersArray.push(myWordArray[i]);
       }
-
 		}
+
 		//set width of answer display--50wide, 10padding, 1border
-    console.log(myWordArray);
-    console.log(lettersArray)
-		var displayWidth = 52 * divcount + 10;
+		var displayWidth = 42 * divcount + 20;
 		displayWidth = displayWidth.toString() + "px";
-		$("#wordDisplay").css("width", displayWidth);
+		$(".wordDisplay").css("width", displayWidth);
+    $('.clickable').on('click', checkLetter);
 	};
 
   //reset the initial conditions to play again
   function myReset(){
+    $('clickable').off('click', checkLetter);
     guessedLetters = [];
     myWordArray = [];
     numWrongGuesses = 0;
+    lettersArray = [];
+    divcount = 0;
+    $('#hangmanSprite').css('left', '0px');
+    $('#topRow, #botRow, #wordDisplay').empty();
+    console.log('position reset to zero');
   }
 
 
@@ -65,10 +76,8 @@ function play() {
   function checkLetter(){
     //currentLetter is placeholder for the id of the letter the user guessed
     currentLetter = this.id;
-    //console.dir(this);
     $(this).addClass('guessed');
     $(this).removeClass('clickable');
-    //console.dir(this);
 
     //check to see whether the user has guessed that letter before
     var guessedFlag = false;
@@ -82,48 +91,44 @@ function play() {
     var correctFlag = false;
     if (guessedFlag === false){
       guessedLetters.push(currentLetter);
+      //check and see if letter is correct
       for (var i = 0; i < lettersArray.length; i++){
         if( currentLetter === lettersArray[i]){
           correctFlag = true;
           console.log('The class to write to is .letter' + currentLetter);
           $('.letter' + currentLetter).append('<p class="letter">' + currentLetter + '</p>');
+          lettersArray.splice(i,1);
         }
       }
+      //if letter is wrong
       if (correctFlag === false){
         numWrongGuesses++;
+        $('#hangmanSprite').css('left', -numWrongGuesses * spriteIncrement + 'px');
+        console.log('position increments');
       }
 
+      //win condition
+      if(lettersArray.length === 0){
+        if(confirm('You win! Play again?')){
+          play();
+        }
+      //loss condition
+      } else if (numWrongGuesses === maxGuesses){
+        if (confirm('You lose! Play again?')){
+          play();
+        }
+      }
     }
-    //console.log(guessedFlag);
-    console.log('checking letter ' + this.id);
   }
 
 
 
   //runs game at page open
 	play();
-  $('.clickable').on('click', checkLetter);
-
-
-
-
-
-
-
-// $('.letterHolder').on('click', myFunction);
-//
-// function myFunction(e){
-//   console.log("I'm sorry Dave.");
-//   console.dir(e);
-//   console.log(e.target.id);
-// }
-// $('#testDiv').click(function(){
-//   $.ajax({url: "test.txt", success: function(placeHolder){
-//     $("#testDiv").html(placeHolder);
-//   }})
-// });
-
-
 
 
 });
+
+
+
+//bug-- once play is called again, it regenerates the keyboard, but the keyboard can't be interacted with anymore, possibly because those keys weren't there at page load?
